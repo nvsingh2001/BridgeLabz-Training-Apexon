593e399935dd5888421f39d54684a3ca75f3db53
nvsingh2001 <nvsingh2001@hotmail.com>
2025-12-06 12:24:23 +0530

    Added: editorconfig

--- OOP/OOP/obj/Release/net10.0/OOP.AssemblyInfo.cs
+++ OOP/OOP/obj/Release/net10.0/OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c195f3f497ed9463cfba8d8467a3530ac74b730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8afe3eee2a527457b3117d7b032e2a1f9cce61e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
